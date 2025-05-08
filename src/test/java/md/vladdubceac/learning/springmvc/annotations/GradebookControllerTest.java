f9118f6d621b657dc022d6cdef4f9cbc1968f260
Vlad <dubceacvlad@gmail.com>
2025-05-09 00:08:07 +0300

    Configuring tests

--- src/test/java/md/vladdubceac/learning/springmvc/annotations/GradebookControllerTest.java
+++ src/test/java/md/vladdubceac/learning/springmvc/annotations/GradebookControllerTest.java
@@ -1,13 +1,16 @@
 package md.vladdubceac.learning.springmvc.annotations;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import md.vladdubceac.learning.springmvc.annotations.models.CollegeStudent;
 import md.vladdubceac.learning.springmvc.annotations.repository.HistoryGradesDao;
 import md.vladdubceac.learning.springmvc.annotations.repository.MathGradesDao;
 import md.vladdubceac.learning.springmvc.annotations.repository.ScienceGradesDao;
 import md.vladdubceac.learning.springmvc.annotations.repository.StudentDao;
 import md.vladdubceac.learning.springmvc.annotations.service.StudentAndGradeService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
@@ -15,9 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 @TestPropertySource("/application-test.properties")
@@ -52,6 +57,15 @@ public class GradebookControllerTest {
     @Autowired
     private StudentAndGradeService studentService;
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private CollegeStudent student;
+
     @Value("${sql.script.create.student}")
     private String sqlAddStudent;
 
@@ -76,6 +90,16 @@ public class GradebookControllerTest {
     @Value("${sql.script.delete.history.grade}")
     private String sqlDeleteHistoryGrade;
 
+    public static final MediaType APPLICATION_JSON_UTFS = MediaType.APPLICATION_JSON;
+
+    @BeforeAll
+    public static void setup(){
+        request = new MockHttpServletRequest();
+        request.setParameter("firstname", "Vlad");
+        request.setParameter("lastname", "Dubceac");
+        request.setParameter("emailAddress", "dubceacvlad@gmail.com");
+    }
+
     @BeforeEach
     public void setupDatabase() {
         jdbc.execute(sqlAddStudent);
@@ -83,12 +107,7 @@ public class GradebookControllerTest {
         jdbc.execute(sqlAddScienceGrade);
         jdbc.execute(sqlAddHistoryGrade);
     }
-
-    @Test
-    public void test(){
-        System.out.println("GradebookControllerTest.test");
-    }
-
+    
     @AfterEach
     public void setupAfterTransaction() {
         jdbc.execute(sqlDeleteStudent);
